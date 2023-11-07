e976a0eab67ab9f219593ea8c09f05d2d0977120
Rene Cruz <renecruz.0711@gmail.com>
2023-11-06 19:23:39 -0600

    Creating project in Spring

---
+++ src/main/java/code/api_compas/ApiCompasApplication.java
@@ -0,0 +1,13 @@
+package code.api_compas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCompasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCompasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/code/api_compas/ApiCompasApplicationTests.java
@@ -0,0 +1,13 @@
+package code.api_compas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCompasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
