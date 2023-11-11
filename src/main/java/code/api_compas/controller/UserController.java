5d76ecb0a99d7f7a6cb6d852ce981fdd3b315e51
Estefania Ortega <zorleck.kongos@gmail.com>
2023-11-11 00:51:13 -0600

    CRUD User

---
+++ src/main/java/code/api_compas/controller/UserController.java
@@ -0,0 +1,69 @@
+package code.api_compas.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import code.api_compas.model.User;
+import code.api_compas.repository.UserRepository;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return this.repository.findAll();
+    }
+
+    @PostMapping
+    public User creatUser(@RequestBody User user) {
+        return this.repository.save(user);
+    }
+
+    //(condicion)?valor1:valor2;
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(
+            @PathVariable("id") String id,
+            @RequestBody User user) {
+        Optional<User> currentData = this.repository.findById(id);
+        if (currentData.isPresent()) {
+            User newUser = currentData.get();
+            newUser.setUsername(user.getUsername());
+            newUser.setEmail(user.getEmail());    
+            newUser.setPassword(user.getPassword());
+            newUser.setCreated_at(user.getCreated_at());
+            newUser.setImage_profile(user.getImage_profile());
+            newUser.setDescription(user.getDescription());
+            newUser.setRole(user.getRole());        
+            return new ResponseEntity<>(this.repository.save(newUser), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<User> deleteUser(@PathVariable("id") String id) {
+    Optional<User> currentData = this.repository.findById(id);
+    if (currentData.isPresent()) {
+      this.repository.deleteById(id);
+      return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    } else {
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+  }
+}
--- src/main/java/code/api_compas/model/User.java
+++ src/main/java/code/api_compas/model/User.java
@@ -17,7 +17,84 @@ public class User {
     private String image_profile;
     private String description;
     private String role;
-    
-    
+
+    public User() {
+    }
+
+    public User(String id, String username, String email, String password, Date created_at, String image_profile,
+            String description, String role) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.created_at = created_at;
+        this.image_profile = image_profile;
+        this.description = description;
+        this.role = role;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    public String getImage_profile() {
+        return image_profile;
+    }
+
+    public void setImage_profile(String image_profile) {
+        this.image_profile = image_profile;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 
 }
